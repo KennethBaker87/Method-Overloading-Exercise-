7da5bf6bb89825c348a8cfb42e6dc8d29e8a40bd
Kenneth Baker <spyksaturn@gmail.com>
2023-02-08 16:27:19 -0500

    initial commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+
+
+            Console.WriteLine(MethodOverload.Add(3, 5));
+            Console.WriteLine(MethodOverload.Add(2.63m, 6.79m));
+            Console.WriteLine(MethodOverload.Add(2, 5, true));
         }
     }
 }
